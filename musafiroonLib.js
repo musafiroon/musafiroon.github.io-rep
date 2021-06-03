be742eb20100bacaba109d6d0e8e8f424f46f80b
musafiroon <73289588+musafiroon@users.noreply.github.com>
2021-06-03 10:44:17 +0530

    Create musafiroonLib.js

---
+++ musafiroonLib.js
@@ -0,0 +1,12 @@
+var random = function(sf, ea) {
+    return (Math.random() * (ea - sf) + sf);
+};
+function itemItemInArray(item, array) {
+    var result = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == item) {
+            result = i;
+        }
+    }
+    return (result);
+};
