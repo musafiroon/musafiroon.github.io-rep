38e7ec8064e09733b52c4ccfb6ac2159ef743094
musafiroon <73289588+musafiroon@users.noreply.github.com>
2021-06-03 10:46:21 +0530

    Update musafiroonLib.js

--- musafiroonLib.js
+++ musafiroonLib.js
@@ -1,7 +1,7 @@
 var random = function(sf, ea) {
     return (Math.random() * (ea - sf) + sf);
 };
-function itemItemInArray(item, array) {
+function noOfItemInArray(item, array) {
     var result = 0;
     for (var i = 0; i < array.length; i++) {
         if (array[i] == item) {
