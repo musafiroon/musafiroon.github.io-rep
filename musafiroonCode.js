2fa7160b5a3d9b35b9ab6dfe16bdcf2af14583d8
musafiroon <73289588+musafiroon@users.noreply.github.com>
2021-06-03 09:04:09 +0530

    Update musafiroonCode.js

--- musafiroonCode.js
+++ musafiroonCode.js
@@ -1,13 +1,13 @@
-var random = function(sf,ea){
-    return(Math.random()*(ea-sf)+sf);
-}
+var random = function(sf, ea) {
+	return (Math.random() * (ea - sf) + sf);
+};
 
 function itemItemInArray(item, array) {
-    var result = 0;
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] == item) {
-            result = i;
-        }
-    }
-    return (result);
+	var result = 0;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] == item) {
+			result = i;
+		}
+	}
+	return (result);
 }
