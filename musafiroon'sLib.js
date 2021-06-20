e94e5d8d103404047df4581d9dbf5b411fbd9a06
musafiroon <73289588+musafiroon@users.noreply.github.com>
2021-06-20 15:59:41 +0530

    Update musafiroon'sLib.js

--- musafiroon'sLib.js
+++ musafiroon'sLib.js
@@ -7,7 +7,7 @@ function round(n){
 };
 
 function noOfItemInArray(item, array) {
-    var result = 0;
+    var result;
     for (var i = 0; i < array.length; i++) {
         if (array[i] == item) {
             result = i;
